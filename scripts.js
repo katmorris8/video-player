9c69ec843c6f0be676ba263ab1750ce9b1be14cd
katmorris8 <kathryn.leigh.morris@gmail.com>
2020-05-02 15:13:04 -0400

    added the skip buttons

--- scripts.js
+++ scripts.js
@@ -21,8 +21,14 @@ function updateButton() {
     toggle.textContent = icon;
 }
 
+function skip() {
+    video.currentTime += Number(this.dataset.skip);
+}
+
 // event listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+
 toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
